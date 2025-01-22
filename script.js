dbfa76b1a860f60755d805e1898d5b12fcd4ea13
davronweb009 <davronweb07@mail.ru>
2025-01-22 17:20:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+ let name = prompt("Ismingizni kiriting")
+  let spaceId = prompt("Space id kiriting")
+ let spacePassword = prompt("Space passwordingizni kiriting")
+
+ console.log(name)
+ console.log(spaceId)
+ console.log(spacePassword)
